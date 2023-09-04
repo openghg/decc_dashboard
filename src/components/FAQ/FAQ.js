6867612ee7c38e5fd22ea9a21781eb967b38a28d
prasad <sutar.prasad@yahoo.com>
2023-09-04 22:43:56 +0100

    updated FAQ

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import QnA from "../QnA/QnA";
-import styles from "../../Dashboard.module.css";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import { Typography } from "@mui/material";
 
 class FAQ extends React.Component {
@@ -14,9 +14,10 @@ class FAQ extends React.Component {
     measured greenhouse gas concentrations at a given location change continuously.`;
 
     const Q2 = "What causes the amount of mixing in the atmosphere to change?";
-    const A2 = `Mixing of greenhouse gases is driven by turbulence. You experience this turbulence every day as gusts of wind.\n
-    - Turbulence tends to be high when winds are strong or when the surface is warm compared to the air higher up in the atmosphere (e.g., on a sunny day, the sun warms the ground more than the air).\n 
-    - Turbulence is weaker when the wind speed is low, or when the ground is cooler than the air (e.g., during a clear night).\n
+    const A2 = `Mixing of greenhouse gases is driven by turbulence. You experience this turbulence every day as gusts of wind.
+    
+    Turbulence tends to be high when winds are strong or when the surface is warm compared to the air higher up in the atmosphere (e.g., on a sunny day, the sun warms the ground more than the air).
+    Turbulence is weaker when the wind speed is low, or when the ground is cooler than the air (e.g., during a clear night).
     During periods of strong turbulence, gases are mixed away from the surface more rapidly than during times with low turbulence, or vice versa. Therefore, when the turbulence is strong and gases are mixed more rapidly, concentrations tend to be lower.`;
 
     const Q3 = "Why do the concentrations increase at night-time?";
@@ -64,23 +65,60 @@ class FAQ extends React.Component {
 
     return (
       <div>
-      <div style={{padding:'5%'}}>
-        <Typography variant="h3">Frequently Asked Questions</Typography>
-      </div>
-        <div className={styles.faqContent}>
-          <QnA number="Q1" split={false} question={Q1} answer={A1} />
-          <QnA number="Q2" split={true} question={Q2} answer={A2} />
-          <QnA number="Q3" split={false} question={Q3} answer={A3} />
-          <QnA number="Q4" split={false} question={Q4} answer={A4} />
-          <QnA number="Q5" split={false} question={Q5} answer={A5} />
-          <QnA number="Q6" split={false} question={Q6} answer={A6} />
-          <QnA number="Q7" split={false} question={Q7} answer={A7} />
-          <QnA number="Q8" split={false} question={Q8} answer={A8} />
-      </div>
-      </div>
-
+        <Typography variant="h3" style={{padding:'1.5%'}} gutterBottom>
+          Frequently Asked Questions
+        </Typography>
+  
+        <Card>
+          <CardContent>
+            <Typography variant="h4">Q1: {Q1}</Typography>
+            <Typography>{A1}</Typography>
+          </CardContent>
+          </Card>
+          <Card>
+          <CardContent>
+            <Typography variant="h4">Q2: {Q2}</Typography>
+            <Typography>{A2}</Typography>
+          </CardContent>
+          </Card>
+          <Card>
+          <CardContent>
+            <Typography variant="h4">Q3: {Q3}</Typography>
+            <Typography>{A3}</Typography>
+          </CardContent>
+          </Card>
+          <Card>
+          <CardContent>
+            <Typography variant="h4">Q4: {Q4}</Typography>
+            <Typography>{A4}</Typography>
+          </CardContent> 
+          </Card>
+          <Card>
+          <CardContent>
+            <Typography variant="h4">Q5: {Q5}</Typography>
+            <Typography>{A5}</Typography>
+          </CardContent>    
+          </Card>
+          <Card>      
+          <CardContent>
+            <Typography variant="h4">Q6: {Q6}</Typography>
+            <Typography>{A6}</Typography>
+          </CardContent>
+          </Card>       
+          <Card>   
+          <CardContent>
+            <Typography variant="h4">Q7: {Q7}</Typography>
+            <Typography>{A7}</Typography>
+          </CardContent>
+          </Card>
+          <Card>
+          <CardContent>
+            <Typography variant="h4">Q8: {Q8}</Typography>
+            <Typography>{A8}</Typography>
+          </CardContent>
+        </Card>
+        </div>
     );
-  }
-}
+  };}
 
 export default FAQ;
